930b196ab22b295474ca5eec4cb857e130bba18f
Floretcl <38957472+floretcl@users.noreply.github.com>
2021-02-03 20:09:50 +0100

    Initial Commit

---
+++ KanaTorii/KanaToriiApp.swift
@@ -0,0 +1,20 @@
+//
+//  KanaToriiApp.swift
+//  KanaTorii
+//
+//  Created by Clément FLORET on 03/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct KanaToriiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
